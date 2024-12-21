bdebb36064c6e804ea02c3a115a93dfcec3d7429
bimaadam <bimavyanisty12@gmail.com>
2024-12-21 15:27:31 +0700

    fix ui mobile and desktop

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,23 +1,24 @@
-import Sidebar from '@/components/Sidebar'
-import React from 'react'
+import Sidebar from "@/components/Sidebar";
 
 const Dashboard = () => {
   return (
-    <div className='flex'>
-      {/* Sidebar with fixed width */}
-      <div className='w-64'>
+    <div className='flex flex-col md:flex-row'>
+      {/* Sidebar dengan lebar tetap */}
+      <div className='w-full md:w-auto min-w-64'>
         <Sidebar />
       </div>
 
-      {/* Main content */}
+      {/* Konten utama */}
       <div className='flex-1 bg-slate-500 min-h-screen p-4'>
-        <h1 className='text-white text-xl mb-4'>Dashboard</h1>
+        <div className="flex items-center mb-4 h-8 me-3">
+        </div>
+        <h1 className='text-white text-xl mb-4 '>Dashboard</h1>
         <div className='p-4 bg-white rounded shadow'>
-          <p>Ini adalah div baru! Sekarang pasti kelihatan 🥰</p>
+          <p>Coming Soon Masih Dalam Pengembangan Oleh Bima Adam</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
